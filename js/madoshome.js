c0caf53e60adb2d578ce47d81002df366a26afe1
jamesamuli <jamesamuli1998@gmail.com>
2022-07-28 17:18:27 +0300

    @media query finished

--- js/madoshome.js
+++ js/madoshome.js
@@ -1,12 +1,11 @@
-var menutoggle = document.querySelector('#amburger');
-var menuHidden = document.querySelector('.navigation_menu_expanded');
-var close = menuHidden.querySelector("#close");
+var menutoggle = document.querySelector('#visible_amburger');
+var menuHidden = document.querySelector('.Menu');
+var close = menuHidden.querySelector(".close_button");
 
 
 menutoggle.addEventListener('click', (e)=>{
     e.stopPropagation();
     menuHidden.classList.toggle('appear')
-
 })
 close.addEventListener('click', (e)=>{
     e.stopPropagation();
